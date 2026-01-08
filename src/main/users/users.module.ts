4479e49797430d53c5d3e25dd34087ca8ab99e89
Al-amin07 <alsujon2001@gmail.com>
2026-01-08 10:22:47 +0600

    first commit

---
+++ src/main/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+
+import { UserRepository } from "./auth.repository";
+import { UsersController } from "./users.controller";
+
+@Module({
+    imports: [],
+    providers: [UserRepository],
+    controllers: [UsersController],
+})
+export class UsersModule {}
